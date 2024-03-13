0282590e6dac504bef59b5249d84a5a69b774f16
Alton <smithiialton@gmail.com>
2024-03-12 21:41:41 -0700

    completed task 1 problem 1

--- pytohnStrings.py
+++ pytohnStrings.py
@@ -4,9 +4,17 @@
 
 # Task 1: Code Correction
 # You are provided with a script that is supposed to format customer names by ensuring the first letter is uppercase and the rest are lowercase, regardless of how the data was entered. However, the script contains errors. Correct the script so that it functions as intended.
+def names(firstName, lastName):
+    print(firstName.capitalize() + " " + lastName.capitalize())
+
+names('john', 'doe')
 
 # Task 2: Email Validation
 # Write a function that checks a list of email addresses for a SaaS application's user accounts. The function should verify that each email contains an "@" symbol and a "." after it, indicating a valid email format. If an email doesn't meet this criterion, print it out for further review.
+
+
+
+
 
 # Task 3: Username Generation
 # Create a script that generates a username for each new user. The username should be a combination of the first three letters of their first name and the first three letters of their last name. If the name is shorter than three letters, use the full name. Ensure all usernames are in lowercase.
